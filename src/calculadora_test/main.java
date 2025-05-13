6c5335dce593c33c044878f5f47e0a9cc03422d8
isajisa <isabel.j.s.2004@gmail.com>
2025-05-13 20:04:21 +0200

    prueba de todos los metodo

--- src/calculadora_test/main.java
+++ src/calculadora_test/main.java
@@ -13,13 +13,19 @@ public class main {
         
         Calculadora calc=new Calculadora();
         
+        System.out.println("-----Prueba método suma-----");
+        System.out.println("suma de 30+20 :" +calc.multipicacion(30, 20));
+        
         System.out.println("-----Prueba método multiplicación-----");
         System.out.println("multiplación de 2x50 :" +calc.multipicacion(2, 50));
         System.out.println("-----Prueba método factorial----- ");
         System.out.println(" factorial de 5 :"+calc.factorial(5));
         
+        System.out.println("-----Prueba método restar-----");
+        System.out.println("restar de 50-20 : " + calc.restar(50, 20));
+        
         System.out.println("-----Prueba método division-----");
-        System.out.println("division de 2x50 : " + calc.dividir(10, 5));
+        System.out.println("division de 10/5 : " + calc.dividir(10, 5));
        
     }
     
